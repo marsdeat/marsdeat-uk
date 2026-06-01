f12c76ba90f3a75ee6e338262eb9e2b9e08cbfaf
Andrew Marsden <atmarsden95@gmail.com>
2026-06-01 20:57:49 +0100

    Try 3

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import { SITE } from "./src/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.marsdeat.uk",
+  site: "https://marsdeat.uk",
   integrations: [
     tailwind({
       applyBaseStyles: false,
